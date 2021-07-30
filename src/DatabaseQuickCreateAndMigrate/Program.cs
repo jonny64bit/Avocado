0df743c61a705c322f0ab50a50daf02ddeeb88d3
Jonathan Dent <jonathan.dent@bristolitcompany.com>
2021-07-30 19:46:36 +0100

    Fixing up seed data indentity insert issue.

--- src/DatabaseQuickCreateAndMigrate/Program.cs
+++ src/DatabaseQuickCreateAndMigrate/Program.cs
@@ -75,6 +75,9 @@ namespace DatabaseQuickCreateAndMigrate
 
         static async Task Seed(DAL context)
         {
+            //Instead of seeding here you could do this in database migrations.
+            //Really if its only data for testing/development i like to keep it out of migrations.
+
             var rawLines = new List<string>
             {
                 "2344,Tommy,Test",
@@ -106,18 +109,27 @@ namespace DatabaseQuickCreateAndMigrate
                 "1248,Pam,Test"
             };
 
-            foreach (var rawLine in rawLines)
+            using (var transaction = await context.Database.BeginTransactionAsync())
             {
-                var segments = rawLine.Split(',');
-                await context.Accounts.AddAsync(new()
-                {
-                    Id = int.Parse(segments[0]),
-                    FirstName = segments[1],
-                    LastName = segments[2]
-                });
-            }
+                await context.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Accounts] ON");
 
-            await context.SaveChangesAsync();
+                foreach (var rawLine in rawLines)
+                {
+                    var segments = rawLine.Split(',');
+                    await context.Accounts.AddAsync(new()
+                    {
+                        Id = int.Parse(segments[0]),
+                        FirstName = segments[1],
+                        LastName = segments[2]
+                    });
+                }
+
+                await context.SaveChangesAsync();
+
+                await context.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Accounts] OFF");
+
+                await transaction.CommitAsync();
+            }
         }
     }
 }
