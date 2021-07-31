5c03bc3d5c03e0c5bda497620545a23ba0a6f923
Jonathan Dent <jonathan.dent@bristolitcompany.com>
2021-07-31 15:36:49 +0100

    Added softdelete to accounts

--- src/Avocado.Database/Models/Account.cs
+++ src/Avocado.Database/Models/Account.cs
@@ -3,11 +3,12 @@ using Pact.Web.Vue.Grid.Interfaces;
 
 namespace Avocado.Database.Models
 {
-    public class Account : IDatabaseObject
+    public class Account : ISoftDelete
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public bool SoftDelete { get; set; }
 
         /////////////
         ///FKs
